1307d0a6ec56a2b1eed4e15dfc27ab4941f6a09f
mfayech <med1502@hotmail.fr>
2026-06-08 13:10:06 +0200

    fix: add missing channelos/pipeline/__init__.py

---
+++ channelos/pipeline/__init__.py
@@ -0,0 +1,8 @@
+from channelos.pipeline.render import (
+    fetch_broll,
+    fetch_broll_multi,
+    render_video,
+    download_video,
+)
+
+__all__ = ["fetch_broll", "fetch_broll_multi", "render_video", "download_video"]
